6b16e2c4c6c13792d811437076cc694c09f1c623
Psiko <roosi80@googlemail.com>
2018-08-02 11:16:27 +0200

    standard.js style

--- js/scenegraph/animation-nodes.js
+++ js/scenegraph/animation-nodes.js
@@ -2,21 +2,21 @@
  * Class representing an Animation
  */
 class AnimationNode {
-    /**
-     * Creates a new AnimationNode
-     * @param {GroupNode} groupNode - The GroupNode to attach to
-     */
-    constructor(groupNode) {
-        this.groupNode = groupNode;
-        this.active = true;
-    }
+  /**
+   * Creates a new AnimationNode
+   * @param {GroupNode} groupNode - The GroupNode to attach to
+   */
+  constructor (groupNode) {
+    this.groupNode = groupNode
+    this.active = true
+  }
 
-    /**
-     * Toggles the active state of the animation node
-     */
-    toggleActive() {
-        this.active = !this.active;
-    }
+  /**
+   * Toggles the active state of the animation node
+   */
+  toggleActive () {
+    this.active = !this.active
+  }
 
 }
 
@@ -25,29 +25,28 @@ class AnimationNode {
  * @extends AnimationNode
  */
 class RotationNode extends AnimationNode {
-    /**
-     * Creates a new RotationNode
-     * @param {GroupNode} groupNode - The group node to attach to
-     * @param {Vector} axis         - The axis to rotate around
-     */
-    constructor(groupNode, axis) {
-        super(groupNode);
-        this.angle = 0;
-        this.axis = axis;
-    }
+  /**
+   * Creates a new RotationNode
+   * @param {GroupNode} groupNode - The group node to attach to
+   * @param {Vector} axis         - The axis to rotate around
+   */
+  constructor (groupNode, axis) {
+    super(groupNode)
+    this.angle = 0
+    this.axis = axis
+  }
 
-    /**
-     * Advances the animation by deltaT
-     * @param  {number} deltaT - The time difference, the animation is advanced by
-     */
-    simulate(deltaT) {
-        // change the matrix of the attached
-        // group node to reflect a rotation
-        if (this.active) {
-            let angle = deltaT / 1000;
-            angle %= 2.0 * Math.PI;
-            this.groupNode.matrix = this.groupNode.matrix.mul(Matrix.rotation(this.axis, angle));
-        }
+  /**
+   * Advances the animation by deltaT
+   * @param  {number} deltaT - The time difference, the animation is advanced by
+   */
+  simulate (deltaT) {
+    // change the matrix of the attached
+    // group node to reflect a rotation
+    if (this.active) {
+      let angle = deltaT / 1000
+      angle %= 2.0 * Math.PI
+      this.groupNode.matrix = this.groupNode.matrix.mul(Matrix.rotation(this.axis, angle))
     }
-
+  }
 }
--- js/scenegraph/nodes.js
+++ js/scenegraph/nodes.js
@@ -2,12 +2,12 @@
  * Class representing a Node in a Scenegraph
  */
 class Node {
-    /**
-     * Accepts a visitor according to the visitor pattern
-     * @param  {Visitor} visitor - The visitor
-     */
-    accept(visitor) {
-    }
+  /**
+   * Accepts a visitor according to the visitor pattern
+   * @param  {Visitor} visitor - The visitor
+   */
+  accept (visitor) {
+  }
 }
 
 /**
@@ -17,31 +17,31 @@ class Node {
  * @extends Node
  */
 class GroupNode extends Node {
-    /**
-     * Constructor
-     * @param  {Matrix} mat - A matrix describing the node's transformation
-     */
-    constructor(mat) {
-        super();
-        this.matrix = mat;
-        this.children = [];
-    }
+  /**
+   * Constructor
+   * @param  {Matrix} mat - A matrix describing the node's transformation
+   */
+  constructor (mat) {
+    super()
+    this.matrix = mat
+    this.children = []
+  }
 
-    /**
-     * Accepts a visitor according to the visitor pattern
-     * @param  {Visitor} visitor - The visitor
-     */
-    accept(visitor) {
-        visitor.visitGroupNode(this);
-    }
+  /**
+   * Accepts a visitor according to the visitor pattern
+   * @param  {Visitor} visitor - The visitor
+   */
+  accept (visitor) {
+    visitor.visitGroupNode(this)
+  }
 
-    /**
-     * Adds a child node
-     * @param {Node} childNode - The child node to add
-     */
-    add(childNode) {
-        this.children.push(childNode);
-    }
+  /**
+   * Adds a child node
+   * @param {Node} childNode - The child node to add
+   */
+  add (childNode) {
+    this.children.push(childNode)
+  }
 }
 
 /**
@@ -49,26 +49,26 @@ class GroupNode extends Node {
  * @extends Node
  */
 class SphereNode extends Node {
-    /**
-     * Creates a new Sphere with center and radius
-     * @param  {Vector} center - The center of the Sphere
-     * @param  {number} radius - The radius of the Sphere
-     * @param  {Vector} color  - The colour of the Sphere
-     */
-    constructor(center, radius, color) {
-        super();
-        this.center = center;
-        this.radius = radius;
-        this.color = color;
-    }
+  /**
+   * Creates a new Sphere with center and radius
+   * @param  {Vector} center - The center of the Sphere
+   * @param  {number} radius - The radius of the Sphere
+   * @param  {Vector} color  - The colour of the Sphere
+   */
+  constructor (center, radius, color) {
+    super()
+    this.center = center
+    this.radius = radius
+    this.color = color
+  }
 
-    /**
-     * Accepts a visitor according to the visitor pattern
-     * @param  {Visitor} visitor - The visitor
-     */
-    accept(visitor) {
-        visitor.visitSphereNode(this);
-    }
+  /**
+   * Accepts a visitor according to the visitor pattern
+   * @param  {Visitor} visitor - The visitor
+   */
+  accept (visitor) {
+    visitor.visitSphereNode(this)
+  }
 }
 
 /**
@@ -76,26 +76,26 @@ class SphereNode extends Node {
  * @extends Node
  */
 class AABoxNode extends Node {
-    /**
-     * Creates an axis aligned box
-     * @param  {Vector} minPoint - The minimum Point
-     * @param  {Vector} maxPoint - The maximum Point
-     * @param  {Vector} color    - The colour of the cube
-     */
-    constructor(minPoint, maxPoint, color) {
-        super();
-        this.minPoint = minPoint;
-        this.maxPoint = maxPoint;
-        this.color = color;
-    }
+  /**
+   * Creates an axis aligned box
+   * @param  {Vector} minPoint - The minimum Point
+   * @param  {Vector} maxPoint - The maximum Point
+   * @param  {Vector} color    - The colour of the cube
+   */
+  constructor (minPoint, maxPoint, color) {
+    super()
+    this.minPoint = minPoint
+    this.maxPoint = maxPoint
+    this.color = color
+  }
 
-    /**
-     * Accepts a visitor according to the visitor pattern
-     * @param  {Visitor} visitor - The visitor
-     */
-    accept(visitor) {
-        visitor.visitAABoxNode(this);
-    }
+  /**
+   * Accepts a visitor according to the visitor pattern
+   * @param  {Visitor} visitor - The visitor
+   */
+  accept (visitor) {
+    visitor.visitAABoxNode(this)
+  }
 }
 
 /**
@@ -103,25 +103,24 @@ class AABoxNode extends Node {
  * @extends Node
  */
 class TextureBoxNode extends Node {
-    /**
-     * Creates an axis aligned box textured box
-     * @param  {Vector} minPoint - The minimum Point
-     * @param  {Vector} maxPoint - The maximum Point
-     * @param  {string} texture  - The image filename for the texture
-     */
-    constructor(minPoint, maxPoint, texture) {
-        super();
-        this.minPoint = minPoint;
-        this.maxPoint = maxPoint;
-        this.texture = texture;
-    }
+  /**
+   * Creates an axis aligned box textured box
+   * @param  {Vector} minPoint - The minimum Point
+   * @param  {Vector} maxPoint - The maximum Point
+   * @param  {string} texture  - The image filename for the texture
+   */
+  constructor (minPoint, maxPoint, texture) {
+    super()
+    this.minPoint = minPoint
+    this.maxPoint = maxPoint
+    this.texture = texture
+  }
 
-    /**
-     * Accepts a visitor according to the visitor pattern
-     * @param  {Visitor} visitor - The visitor
-     */
-    accept(visitor) {
-        visitor.visitTextureBoxNode(this);
-
-    }
+  /**
+   * Accepts a visitor according to the visitor pattern
+   * @param  {Visitor} visitor - The visitor
+   */
+  accept (visitor) {
+    visitor.visitTextureBoxNode(this)
+  }
 }
