34881ca87f1ca08a89f8d8c5f8d9f40297ad300f
Marius Spangenberger <s346235@stud-mail.uni-wuerzburg.de>
2018-08-09 22:00:35 +0200

    add loadDialog

---
+++ js/scenegraph/loadSave.js
@@ -0,0 +1,10 @@
+/* global $ */
+
+function loadScenegraph (event) {
+  let file = $('#loadScenegraph')[0].children[0].files[0]
+  console.log(file.name)
+}
+
+function saveScenegraphToXml () {
+
+}
