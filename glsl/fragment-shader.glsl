c6b0ff06ef7e95ed7c28cf608b2fa906abdcb30b
Lennart Fries <lennart.fries@stud-mail.uni-wuerzburg.de>
2018-08-20 17:43:03 +0200

    more stuff

--- glsl/fragment-shader.glsl
+++ glsl/fragment-shader.glsl
@@ -23,6 +23,8 @@ const float shininess = 4.0;
 
 void main(void) {
     vec4 raw_color;
+    float LightPosDiffuse;
+    float LightPosSpecular;
 
 
     if (textured == 1) {
@@ -37,32 +39,27 @@ void main(void) {
     vec3 color = coefficientAmbient * base_color;
 
     vec3 normal = normalize(v_normal);
+    vec3 viewDir = normalize(-v_position2.xyz);
 
-    vec3 lightDir[lightPosCount];
+    vec3 lightDir;
+    float lj;
 
     for(int i = 0; i < lightPosCount; i++) {
-        lightDir[i] = normalize(lightPositions[i] - v_position2.xyz);
+        lightDir = normalize(lightPositions[i] - v_position2.xyz);
+        lj = length(lightPositions[i] - v_position2.xyz);
+        vec3 reflectDir = reflect(-lightDir[i],normal);
+        float specAngle = max(dot(reflectDir, viewDir),0.0);
+        float specPow = pow(specAngle, shininess);
+        LightPosDiffuse += lj * max(dot(lightDir[i], normal), 0.0);
+        LightPosSpecular += lj * specPow;
     }
 
-    // diffuse
-    float LightPosDiffuse;
-    for(int i = 0; i < lightPosCount; i++) {
-        LightPosDiffuse += lightPositions[i] * max(dot(lightDir[i], normal), 0.0);
-    }
+   // diffuse
     float lambertian = coefficientDiffuse * LightPosDiffuse;
     vec3 diffuseColor = lambertian * base_color;
     color += diffuseColor;
 
     //specular
-    float LightPosSpecular;
-    vec3 viewDir = normalize(-v_position2.xyz);
-
-    for(int i = 0; i < lightPosCount; i++) {
-        vec3 reflectDir = reflect(-lightDir[i],normal);
-        float specAngle = max(dot(reflectDir, viewDir),0.0);
-        float specPow = pow(specAngle, shininess);
-        LightPosSpecular += lightPositions[i] * specPow;
-    }
     float specular = coefficientSpecular * LightPosSpecular;
     vec3 specularVector =  specular * base_color;
     color += specularVector;
