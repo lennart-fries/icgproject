6ddd74de75553ad220b355c1235ef04648f1690d
Lennart Fries <lennart.fries@stud-mail.uni-wuerzburg.de>
2018-08-24 19:26:34 +0200

    Add new Phong for Raytracing (still needs debugging)

--- js/ray/phong.js
+++ js/ray/phong.js
@@ -10,6 +10,54 @@
 import { Vector } from '../primitives/vector.js'
 
 export function phong (objColor, intersection, lightPositions, shininess, cameraPosition) {
+  let coefficientAmbient = 0.3
+  let coefficientDiffuse = 0.6
+  let coefficientSpecular = 1.5
+  let baseColor = objColor
+  // ??? need to get that right
+  let vertexNormal = intersection.normal()
+  let vertexPosition = intersection
+
+  // lightPositions
+  // cameraPosition
+  // shininess
+
+  let lightDirection = (lightPositions[i].sub(vertexPosition)).normalised()
+
+  // ambient new
+
+  let ambientColor = coefficientAmbient * baseColor
+
+  let ambientVec = new Vector(ambientColor, ambientColor, ambientColor, 0)
+  objColor = objColor.add(ambientVec)
+
+  // diffuse new
+
+  let diffuseSum = 0
+  let specularSum = 0
+  let viewDirection = intersection.mul(-1).normalised()
+
+  for (let i = 0; i < lightPositions.size; i++) {
+    let lj = (lightPositions.sub(vertexPosition)).length * lightPositions[i].w()
+    diffuseSum += lj * vertexNormal.dot(lightDirection)
+    // specular
+    let reflectDirection = 1.5 // still needs to be done
+    let specularTerm = Math.pow(viewDirection.dot(reflectDirection), shininess)
+    specularSum += lj * specularTerm
+  }
+
+  let diffuseLambertian = coefficientDiffuse * diffuseSum
+  let diffuseVec = diffuseLambertian * baseColor
+  objColor = objColor.add(diffuseVec)
+
+  // specular new
+  let specularLambertian = coefficientSpecular * specularSum
+  let specularVec = specularLambertian * baseColor
+  objColor = objColor.add(specularVec)
+
+  return objColor
+
+  // altes phong
   let color = objColor
 
   let ka = 0.6
