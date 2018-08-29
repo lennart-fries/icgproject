b9b817863b4d05ac702d73596ece73350410d317
Psiko <roosi80@googlemail.com>
2018-08-29 22:48:28 +0200

    reverse debug changes

--- js/main.js
+++ js/main.js
@@ -45,21 +45,21 @@ const cube = new AABoxNode(
 )
 gn2.add(cube)
 
-const light1 = new LightNode(new Vector(-10, 3, -3, 1), 0.2)
-const lsphere1 = new SphereNode(new Vector(-10, 3, -3, 1), 0.1, new Vector(1, 1, 1, 1))
+const light1 = new LightNode(new Vector(-10, 3, 3, 1), 0.2)
+// const lsphere1 = new SphereNode(new Vector(-10, 3, -3, 1), 0.1, new Vector(1, 1, 1, 1))
 gn1.add(light1)
-gn1.add(lsphere1)
-const light2 = new LightNode(new Vector(10, 3, -3, 1), 0.2)
-const lsphere2 = new SphereNode(new Vector(10, 3, -3, 1), 0.1, new Vector(1, 1, 1, 1))
+// gn1.add(lsphere1)
+const light2 = new LightNode(new Vector(10, 3, 3, 1), 0.2)
+// const lsphere2 = new SphereNode(new Vector(10, 3, -3, 1), 0.1, new Vector(1, 1, 1, 1))
 gn1.add(light2)
-gn1.add(lsphere2)
+// gn1.add(lsphere2)
 
 const cameraNode = new CameraNode(new Vector(0, 0, 10, 1), new Vector(0, 0, 0, 1), new Vector(0, 1, 0, 0), 60, 1, 0.1, 100)
 gn1.add(cameraNode)
 
 let animationNodes = [
   new AnimationNode(gn2, 1.0, true, new Vector(0, 0, 1, 0), Matrix.rotation),
-  //new BackAndForthAnimationNode(gn3, 1.0, true, new Vector(0, 0, 1, 0), Matrix.translation, 3, 1.5)
+  new BackAndForthAnimationNode(gn3, 1.0, true, new Vector(0, 0, 1, 0), Matrix.translation, 3, 1.5)
 ]
 
 /**
