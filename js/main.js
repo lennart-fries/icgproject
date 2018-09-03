5a5b29f7133864857ab8740e71b2c386d4fecf1b
Lennart Fries <lennart.fries@stud-mail.uni-wuerzburg.de>
2018-09-03 11:02:13 +0200

    map something

--- js/main.js
+++ js/main.js
@@ -53,8 +53,8 @@ const cube = new AABoxNode(
   new Vector(1, 1, 1, 1),
   colorVector,
   new Vector(0.3, 0.6, 1.5, 4),
-  'assets/diamond_ore.png',
-  'assets/cube.png'
+  'assets/fieldstone.jpg',
+  'assets/fieldstone-normal.jpg'
 )
 gn2.add(cube)
 
